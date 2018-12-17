7421203b4965de27e294aefaa48cae8c91a49b6a
jelitasm <43837893+jelitasm@users.noreply.github.com>
2018-12-17 12:12:12 +0700

    add tiket

--- app/src/main/java/td/fransiska/ti3a_17_19_tugasbesar/Rest/ApiClient.java
+++ app/src/main/java/td/fransiska/ti3a_17_19_tugasbesar/Rest/ApiClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://10.212.117.162/travel_server/index.php/";
+    public static final String BASE_URL = "http://172.20.10.2/travel_server/index.php/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
